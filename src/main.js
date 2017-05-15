de93bb444060843db4a25a2ffe3430b46671a94b
nicholasroberson <roberson@grinnell.edu>
2017-05-14 21:34:17 -0500

    css added and more functionalities

--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
-var user, access_token;
-var run = function() {
-  var u = JSON.parse(sessionStorage.getItem('user'));
-  access_token = sessionStorage.getItem('OAuth');
-  user = u;
-  console.log(user);
-  listPlaylists();
+var access_token = sessionStorage.getItem('OAuth');
+var user = JSON.parse(sessionStorage.getItem('user'));
+
+// SVG ELEMENTS
+
+var topPlaylists = d3.select('top_playlists');
+
+var generateTopPlaylists = function() {
+  topPlaylists.attr("class","active");
+  console.log("generating user's top playlists.");
+  var playlists = listPlaylists();
 };
-
-var listPlaylists = function() {
-
-  var base_url = "https://api.spotify.com/v1/users/" + user.id + "/playlists";
-
-  var call_url = base_url + '?' + $.param({
-    'user_id' : user.id,
-    'access_token' : access_token,
-    'limit' : 15,
-  });
-  $.ajax({
-    url: call_url,
-    dataType: "json",
-    type : "GET",
-    success : function(result) {
-      console.log(result);
-    }
-  });
-}
-
-run();
