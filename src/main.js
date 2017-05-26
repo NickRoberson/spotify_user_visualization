b7d631612f7d914afcaa9f6919b3e713c74d989a
nicholasroberson <roberson@grinnell.edu>
2017-05-26 12:20:01 -0500

    fixed user graph, added code for bar graph of genres

--- src/main.js
+++ src/main.js
@@ -6,18 +6,19 @@ var user = JSON.parse(sessionStorage.getItem('user'));
 var MAX_ARTISTS = 50;
 var MAX_TRACKS = 50;
 var MAX_PLAYLISTS = 50;
-var RANGE_ARTIST_GRAPH = 4;
-var DEPTH_USER_GRAPH = 4;
-var userGraph;
-var topPlaylists;
-var topTracks;
-var topArtists;
+var RANGE_ARTIST_GRAPH = 6;
+var DEPTH_USER_GRAPH = 3;
 var list_area;
 
-var artists_graph = [];
+// DATA
+var playlists;
+var topTracks;
+var topArtists;
 var graph = {};
 graph.nodes = [];
 graph.links = [];
+
+//STARTUP FUNCTION TO INITIALIZE THE PAGE
 startup();
 /*
 SUNBURST CHART FOR TOP ARTIST GENRES AND TOP SONGS ANALYSIS
@@ -28,12 +29,19 @@ function startup() {
   list_area = d3.select('#song_area');
   console.log("Access Token = " + access_token);
   console.log(user);
+  $('#user_graph').hide();
+
+  // PRELOAD DATA FOR USER
+  meAllPlaylists();
+  populateArtistGraph();
+
+  $('#user_graph').show();
 
   // TABS
   topPlaylists = d3.select('#playlists')
                         .on("click",function() {
                             console.log("Getting playlists for user.");
-                            meAllPlaylists();
+                            addItems(playlists);
                         });
   topPlaylists = d3.select('#top_songs')
                         .on("click",function() {
@@ -48,8 +56,10 @@ function startup() {
   userGraph = d3.select('#user_graph')
                       .on('click', function() {
                           console.log("Generating User graph.");
-                          populateArtistGraph();
-                          //makeUserGraph();
+                          for(node of artists_graph) {
+                            console.log(node.node + " : " + node.child);
+                          }
+                          makeUserGraph();
                         });
   //makeUserGraph();
 }
@@ -59,7 +69,6 @@ function startup() {
 function meAllPlaylists() {
   console.log("List Playlists");
   var base_url = "https://api.spotify.com/v1/me/playlists";
-  var playlists;
   var call_url = base_url + '?' + $.param({
     'limit' : MAX_PLAYLISTS
   });  $.ajax({
@@ -71,7 +80,7 @@ function meAllPlaylists() {
     type : "GET",
     success : function(result) {
       console.log(result);
-      addItems(result);
+      playlists = result;
     }
   });
 };
@@ -96,7 +105,7 @@ function getPlaylistSongs(playlist_id) {
 
 function listSongs() {
   console.log("List Songs");
-  var base_url = "https://api.spotify.com/v1/me/tracks";
+  var base_url = "https://api.spotify.com/v1/me/top/tracks";
   var call_url = base_url + '?' + $.param({
     'limit' : MAX_TRACKS,
     'time_range':'long_term',
@@ -186,17 +195,23 @@ function makeUserGraph() {
 
   list_area.selectAll('#list_item').remove();
   d3.selectAll('svg').remove();
+  d3.selectAll('#graph').remove();
+
   var width = screen.width;
   var height = screen.height;
 
   var svg = d3.select('body').append('svg')
       .attr('width', width)
-      .attr('height', height);
+      .attr('height', height)
+      .attr('id','graph');
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.id; })
+                                 .distance(function(d) { var dist = 150 - 70*(d.source.depth);
+                                                        return dist;})
+                                 .strength(1))
     .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("center", d3.forceCenter(width / 2, height / 2 - 100));
 
   var link = svg.append("g")
       .attr("class", "links")
@@ -212,12 +227,22 @@ var simulation = d3.forceSimulation()
     .data(graph.nodes)
     .enter().append("circle")
       .attr("r", function(d) {
-        if (d.id == user.display_name) { return 50; }
-        else { return 10; }
+        return 50 - d.depth*15;
        })
        .attr("fill", function(d) {
          if (d.id == user.display_name) { return "#84bd00"; }
          else { return "#686868"; }
+       })
+       .on('click', function(d) {
+         d3.select(this).transition().duration(300)
+           .attr("r", 100);
+       })
+       .on('dblclick', function(d) {
+         var r;
+         if (d.id == user.display_name) { r = 50; }
+         else { r = 10; }
+         d3.select(this).transition().duration(300)
+           .attr("r", r);
        })
        .call(d3.drag()
           .on("start", dragstarted)
@@ -258,18 +283,16 @@ function dragged(d) {
 
 function dragended(d) {
   if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+  d.fixed = true;
 }
 }
 
 // depth set to 3
 function populateArtistGraph() {
   console.log("populateArtistGraph()");
-  var artists = [];
   var base_url = "https://api.spotify.com/v1/me/top/artists";
   var call_url = base_url + '?' + $.param({
-    'limit': 5,
+    'limit': RANGE_ARTIST_GRAPH,
     'time_range':'short_term',
   });
   $.ajax({
@@ -280,71 +303,143 @@ function populateArtistGraph() {
     dataType: "json",
     type : "GET",
     success : function(result) {
-      var artists = result.items;
-      console.log(artists);
-      graph.nodes.push({'id' : user.display_name });
+      var data = result.items;
+      //make node for user
+      graph.nodes.push({ 'id' : user.display_name,
+                         'data' : user,
+                         'depth' : 0});
       for(i = 0; i < RANGE_ARTIST_GRAPH; i++) {
-        // PUSH NODE TO GRAPH
-        graph.nodes.push({'id' : artists[i].name});
-        graph.links.push({source : user.display_name,
-                          target : artists[i].name,
-                          depth : 1});
-       }
-       getRelatedArtists(artists, 2);
+        var new_artist = data[i];
+        // make link for current users realted artists
+        graph.links.push({ 'source' : user.display_name,
+                          'target' : new_artist.name });
+        // make node from current user to new artists
+        graph.nodes.push({ 'id' : new_artist.name,
+                           'data' : new_artist,
+                           'depth' : 1});
+        getRelatedArtists(data[i], 2);
+      }
     }
   });
-  //console.log(artists_graph);
-  setTimeout(function() {
-    console.log(graph);
-    makeUserGraph();
-    //addItems({'items' :artists_graph });
-  }, 1000);
 }
 
 
 
-function getRelatedArtists(artists, depth) {
+function getRelatedArtists(artist, depth) {
   console.log("getRelatedArtists()");
-  if (depth < DEPTH_USER_GRAPH) {
-    console.log("Getting related artists.");
-    for (i = 0; i < RANGE_ARTIST_GRAPH; i++) {
-      var name = artists[i].name;
-      // PUSH NODES TO GRAPH
-      graph.nodes.push({'id' : artists[i].name});
-      artists_graph.push(artists[i]);
-      //console.log(i + ": " + artists[i].name + " : " + artists[i].id);
-      var call_url = "https://api.spotify.com/v1/artists/" + artists[i].id + "/related-artists";
-      $.ajax({
-        url: call_url,
-        dataType: "json",
-        type : "GET",
-        success : function(result) {
-          var artists = result.artists;
-          for (i = 0; i < RANGE_ARTIST_GRAPH; i++) {
-            graph.links.push({source : name,
-                              target : artists[i].name,
-                              depth : depth});
+    var call_url = "https://api.spotify.com/v1/artists/" + artist.id + "/related-artists";
+    $.ajax({
+      url: call_url,
+      dataType: "json",
+      type : "GET",
+      success : function(result) {
+        var data = result.artists;
+        var new_depth = depth + 1;
+        for (i = 0; i < RANGE_ARTIST_GRAPH; i++) {
+          if (depth <= DEPTH_USER_GRAPH) {
+            var new_artist = data[i];
+            // make node for new artist
+            graph.nodes.push({ 'id' : new_artist.name,
+                                'data' : new_artist,
+                                'depth' : new_depth});
+            // make link from passed artist to new artist
+            graph.links.push({ 'source' : artist.name,
+                              'target' : new_artist.name });
+            getRelatedArtists(data[i],new_depth);
           }
-          var artists = result.artists;
-          getRelatedArtists(artists,depth+1);
         }
-      });
-    }
-  } else {
-      var name;
-      for (i = 0; i < RANGE_ARTIST_GRAPH; i++) {
-        //artists[i].depth = depth;
-        name = artists[i].name;
-        graph.nodes.push({'id' : artists[i].name});
-        graph.links.push({source : name,
-                          target : artists[i].name,
-                          depth : depth});
-        artists_graph.push(artists[i]);
       }
-    }
+    });
 };
 
 
-function bucket(items) {
 
+
+var addGenreBarChart(genres) {
+
+// Parameters for our plot
+var svg_width = 800;
+var svg_height = 800;
+var left_margin = 90;
+var text_height = 12;
+var margin = 40;
+var plot_width = svg_width - margin - left_margin;
+var plot_height = svg_height - 2 * margin;
+
+// Create the SVG
+var svg = d3.select('#plot').append('svg')
+            .attr('width', svg_width)
+            .attr('height', svg_height);
+
+// Make an x scale and axis for our term bars
+var xscale = d3.scaleBand()
+               .paddingInner(0.3)
+               .paddingOuter(0.3)
+               .domain(Object.keys(word_counts))
+               .range([left_margin, left_margin + plot_width]);
+
+var xaxis = d3.axisBottom(xscale);
+
+// Make a y scale and axis for our percentages
+var yscale = d3.scaleLinear()
+               .domain([0, 0.01])
+               .range([margin + plot_height, margin]);
+
+var yaxis = d3.axisLeft(yscale).tickFormat(d3.format(".1%"));
+
+// Add the x axis
+svg.append('g')
+   .attr('class', 'xaxis')
+   .attr('transform', 'translate(0, ' + (margin + plot_height) + ')')
+   .call(xaxis);
+
+// Add the y axis
+svg.append('g')
+   .attr('class', 'yaxis')
+   .attr('transform', 'translate(' + left_margin + ', 0)')
+   .call(yaxis);
+
+// Add the x axis label
+svg.append('text')
+   .attr('class', 'xaxis-label')
+   .attr('transform', 'translate(' + (left_margin + plot_width/2) + ', ' + (svg_height) + ')')
+   .attr('text-anchor', 'middle')
+   .text('Term');
+
+// Add the y axis label
+svg.append('text')
+   .attr('class', 'yaxis-label')
+   .attr('transform', 'translate(' + text_height + ', ' + (margin + plot_height/2) + ') rotate(-90)')
+   .attr('text-anchor', 'middle')
+   .text('Percent of Tweets Containing Term');
+}
+
+// code to make bar chart of genres for users artists
+function update_plot() {
+  // Update our yscale
+  yscale.domain([0, Math.max(0.01, d3.max(Object.values(word_counts)) / tweet_count)])
+        .nice();
+
+  // Re-generate the y axis with our new y scale
+  d3.select('g.yaxis')
+    .transition().duration(500) // Use a transition so the y axis changes smoothly
+      .call(yaxis);
+
+  // Make a data join between rectangles and our word count data.
+  // We use the d3.entries function to transform our object:
+  // {'Happy': 0, 'Sad': 0} becomes [{key: 'Happy', value: 0}, {key: 'Sad', value: 0}]
+  var join = svg.selectAll('rect').data(d3.entries(genres));
+
+  join.enter() // For new bars, create a rect with zero height at the bottom of the bar chart
+        .append('rect')
+        .attr('class', d => 'bar ' + d.key)
+        .attr('x', d => xscale(d.key))
+        .attr('y', yscale(0))
+        .attr('height', 0)
+        .attr('width', xscale.bandwidth())
+      .merge(join) // Update *all* rects to be the right height, but use a transition
+        .transition().duration(500)
+        // Move the bar down 0.5 so the bar outline overlaps the axis line
+        .attr('y', d => yscale(d.value / tweet_count) + 0.5)
+        .attr('height', d => yscale(0) - yscale(d.value / tweet_count));
 }
